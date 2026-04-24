4bec07770df19157e84e778c6791644b62ad6102
riri16indi <49254278+riri16indi@users.noreply.github.com>
2026-04-24 15:42:33 +0700

    Update Plot.py

--- Plot.py
+++ Plot.py
@@ -2,18 +2,15 @@ import socket
 import numpy as np
 import matplotlib.pyplot as plt
 
-# --- KONFIGURASI JARINGAN ---
 UDP_IP = "0.0.0.0"
 UDP_PORT = 1234
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
-# --- VARIABEL PENYIMPAN DATA ---
-JUMLAH_SAMPEL = 100 # Berapa banyak data yang direkam sebelum grafik muncul
+JUMLAH_SAMPEL = 100
 raw_data_list = []
 filtered_data_list = []
 
-# --- FUNGSI PARTICLE FILTER (Skenario A) ---
 N_PARTICLES = 1000
 particles = np.zeros(N_PARTICLES)
 weights = np.ones(N_PARTICLES) / N_PARTICLES
@@ -29,7 +26,7 @@ def run_particle_filter(raw_val):
     weights *= np.exp(-((particles - raw_val)**2) / 0.1)
     
     sum_w = sum(weights)
-    if sum_w == 0: sum_w = 1e-9 # Pengaman NaN
+    if sum_w == 0: sum_w = 1e-9
     weights /= sum_w
     
     filtered_val = np.sum(particles * weights)
@@ -43,7 +40,6 @@ print(f"Merekam {JUMLAH_SAMPEL} sampel data... Silakan goyangkan sensor IMU Anda
 
 skenario_saat_ini = "Unknown"
 
-# --- PROSES PEREKAMAN DATA ---
 while len(raw_data_list) < JUMLAH_SAMPEL:
     data, addr = sock.recvfrom(1024)
     payload = data.decode('utf-8').split(',')
@@ -59,7 +55,7 @@ while len(raw_data_list) < JUMLAH_SAMPEL:
         
     elif skenario_saat_ini == 'B':
         filtered_x = float(payload[2])
-        raw_x = float(payload[3]) # Mengambil raw data yang baru kita tambahkan di Arduino
+        raw_x = float(payload[3])
         
         raw_data_list.append(raw_x)
         filtered_data_list.append(filtered_x)
@@ -67,7 +63,6 @@ while len(raw_data_list) < JUMLAH_SAMPEL:
 
 print("Perekaman selesai! Menampilkan grafik...")
 
-# --- PROSES MENGGAMBAR GRAFIK UNTUK LAPORAN ---
 plt.figure(figsize=(10, 5))
 plt.plot(raw_data_list, label='Raw Data (Sinyal Mentah Kasar)', color='lightgray', linestyle='--')
 
@@ -82,4 +77,4 @@ plt.xlabel("Sampel Waktu")
 plt.ylabel("Akselerasi (Sumbu X)")
 plt.legend()
 plt.grid(True)
-plt.show() # Ini akan memunculkan jendela grafik
+plt.show()
